d0670bbdd47f77b228d2524a344ab85bdd99dd00
JonathanM11 <Jonathan.manda11@gmail.com>
2024-11-07 12:42:50 -0600

    API Connected (Does not gather correct data)

--- notificationEMR.py
+++ notificationEMR.py
@@ -1,9 +1,30 @@
-# This is just the template
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+import requests
+
+def pullClickUp():
+    apiToken = 'pk_81318893_U3BKRQVH2XL48GARBM10W8SYG7RJ6MCU'
+    listID = '901100495428'
+
+    api_url = f'https://api.clickup.com/api/v2/list/{listID}/task'
+    headers = {
+        'Authorization': apiToken,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.get(api_url, headers=headers)
+    if response.status_code == 200:
+        tasks = response.json().get('tasks', [])
+        for task in tasks:
+            print(f"Task ID: {task['id']}")
+            print(f"Task Name: {task['name']}")
+            print(f"Status: {task['status']['status']}")
+            print("=" * 30)
+    else:
+        print(f"Failed to retrieve tasks: {response.status_code} - {response.text}")
 
 def send_email(receiver_email, subject, body, smtp_server, port):
     try:
@@ -41,5 +62,5 @@ emailServer = "smtp.gmail.com"
 emailPort = 587
 
 
-
-send_email(receiver, emailSubject, emailBody, emailServer, emailPort)
+pullClickUp()
+#send_email(receiver, emailSubject, emailBody, emailServer, emailPort)
